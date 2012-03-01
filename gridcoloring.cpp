872fc898c039034825c0c87e682a61f4e650c1d1
Matthew <mgavin2@tigers.lsu.edu>
2012-03-01 16:04:48 -0600

    Update gridcoloring.cpp

--- gridcoloring.cpp
+++ gridcoloring.cpp
@@ -30,10 +30,14 @@
 using namespace std;
 
 /*global variables*/
-int checkx[] = { 1, 1, 0, -1, -1, -1,  0,  1};
-int checky[] = { 0, 1, 1,  1,  0, -1, -1, -1};
+int checkx[] = { 1,  0,  -1,   0  };
+int checky[] = { 0,  1,    0,  -1 } ;
+int checkx2[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+int checky2[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 int numLines = 0;
-char grid[31][81];
+char grid[35][82];
+string underlines;
+char WALL;
 /*global variables*/
 
 void getInput()
@@ -41,21 +45,22 @@ void getInput()
     //get input
     int p = 1;
   
-    for (int i = 0; i < 31; ++i)
-        for (int j = 0; j < 81; ++j)
+    for (int i = 0; i < 32; ++i)
+        for (int j = 0; j < 82; ++j)
             grid[i][j] = ' ';
 
     string temp;
     getline(cin, temp);
     
-    while (temp.find("_") == string::npos)
+    while (temp.find("_") == string::npos && p < 34)
     { 
-        for (int z = 0; z < temp.size(); ++z)
+        for (int z = 0; z < temp.size() && z < 81; ++z)
             grid[p][z+1] = temp[z];
-        grid[p][temp.size()+1] = '\0';
+        //grid[p][temp.size()+1] = '\0';
         getline(cin, temp);
         ++p;
     }
+    underlines = temp;
     numLines = p-1;
 }
 
@@ -63,8 +68,11 @@ bool isWall(int x, int y)
 {
     for (int i = 0; i < 8; ++i)
     {
-        if (grid[x+checkx[i]][y+checky[i]] == '_')
-            return true;
+        if (grid[x+checkx2[i]][y+checky2[i]] == '_')
+        {
+			WALL = grid[x][y];
+			return true;
+		}
     }
 
     return false;
@@ -74,13 +82,16 @@ bool isWall(int x, int y)
 
 void floodfill(int x, int y, char c)
 {
-    for (int i = 0; i < 8; ++i)
+    for (int i = 0; i < 4; ++i)
     {
-        if (grid[x+checkx[i]][y+checky[i]] != ' ')
-        {
-            grid[x+checkx[i]][y+checky[i]] = c;
-            floodfill(x+checkx[i], y+checky[i], c);
-        }
+      if (x+checkx[i] < 0 || x+checkx[i] > 31 ||
+		  y+checky[i] < 0 || y+checky[i] > 81)
+		continue;
+      if (grid[x+checkx[i]][y+checky[i]] == ' ')
+	  {
+		grid[x+checkx[i]][y+checky[i]] = c;
+		floodfill(x+checkx[i], y+checky[i], c);
+	  }
     }
 }
 
@@ -89,12 +100,12 @@ void process()
     //process input
     char c;
     //floodfill the outside
-    floodfill(1, 1, '_');
-    return;
+    floodfill(0, 0, '_');
+
     for (int i = 1; i <= numLines; ++i)
         for (int j = 1; grid[i][j] != '\0'; ++j)
         {
-            if (grid[i][j] != ' ' && !isWall(i, j))
+            if (grid[i][j] != ' ' && !isWall(i, j) && grid[i][j] != WALL)
             {
                 c = grid[i][j];
                 floodfill(i, j, c);
@@ -111,36 +122,42 @@ int main()
     while (moreToDo = !cin.eof()) //more to do, however you want to express end of input
     {
         getInput();
-
+		/*
         for (int i = 1; i <= numLines; ++i)
         {
             string output;
-            for (int j = 1; grid[i][j] != '\0'; ++j)
+            for (int j = 1; grid[i][j] != '\0' && j < 81; ++j)
                 output += grid[i][j];
             cout << output << endl;
+			
         }
-        
-
+		*/
         process();
-        break;
+		
+
         /*output*/
         for (int i = 1; i <= numLines; ++i)
         {
             string output;
-            for (int j = 1; grid[i][j] != '\0'; ++j)
+            for (int j = 1; grid[i][j] != '\0' && j < 81; ++j)
                 output += grid[i][j];
             for (int j = 0; j < output.length(); ++j)
                 if (output.at(j) == '_')
                     output.at(j) = ' ';
-            cout << output << endl;
-
+			cout << output;
+			//	if (i != numLines)
+				cout << endl;
         }
         /*output*/
-        getline(cin, temp);
-        cout << temp;
-        getline(cin, temp);
+		cout << underlines << endl;
+		do
+		{
+			getline(cin, temp);
+			cout << temp << endl;
+        } while (temp.find(" ") != string::npos &&  temp.find("_") != string::npos && !cin.eof());
         
         numLines = 0;
+		WALL = ' ';
     }
 
     return 0;
