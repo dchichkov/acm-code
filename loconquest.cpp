b2c4cd779afdbe0ac783eabfabbe34edcb7f398f
Matthew <mgavin2@tigers.lsu.edu>
2012-07-30 17:09:02 -0500

    another one :)

---
+++ loconquest.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+#define DEBUG
+//#undef DEBUG //uncomment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#else
+#define debug(a, end)
+#endif
+
+typedef pair<int, int> point;
+typedef long long int64; //for clarity
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } //change min
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //change max
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+map<string, int> countries;
+/*global variables*/
+
+int main()
+{
+    int num_cases = 0;
+    scanf("%d", &num_cases);
+    char coun[80];
+    REP(i, num_cases)
+    {
+        scanf("%s%*[^\n]", coun); scanf("%*[\n]");
+        countries[coun]++;
+        CL(coun, 0);
+    }
+
+    for (map<string, int>::iterator it = countries.begin(); it != countries.end(); ++it)
+        printf("%s %d\n", it->first.c_str(), it->second);
+    
+    return 0;
+}
