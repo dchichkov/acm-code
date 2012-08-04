3f30e212da423742ac94b8a860faaf35afb8a584
Matthew <mgavin2@tigers.lsu.edu>
2012-08-04 13:24:11 -0500

    freaking cant break 0.12

--- ofcands.c
+++ ofcands.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DEBUG
-#undef DEBUG //uncomment this line to pull out print statements
-#ifdef DEBUG
-#define TAB '\t'
-#define debug(a, end) cout << #a << ": " << a << end
-#else
-#define debug(a, end)
-#endif
-
-#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
-#define REP(i,n) FOR(i,0,n)    
-#define CL(a,b) memset(a,b,sizeof(a))
-
 struct shape {
     int px, py;
     int length;
     char type[7];
     int r, g, b;
-    bool is_in_circ, is_on_circ, is_in_squa, is_on_squa;
     int ox, oy;
 };
 
+#define REP(a, b) for ((a) = 0; (a) < (b); ++(a))
+#define debug(a, b)
+
 /*global variables*/
 int num_cases, num_shapes, num_queries;
-shape shapes[102];
+struct shape shapes[102];
 /*global variables*/
 
 
 void dump()
 {
-    //dump data
+    int i;
     REP(i, num_shapes)
     {
         debug(shapes[i].type, endl);
@@ -52,9 +41,9 @@ int main()
         ++count;
         printf("Case %d:\n", count);
         {
-            //int a = 0, b = 0;
             
             scanf("%d %d", &num_shapes, &num_queries);
+            int i;
             REP(i, num_shapes)
             {
                 scanf("%6s %d %d %d %d %d %d", shapes[i].type,
@@ -67,28 +56,19 @@ int main()
                 shapes[i].ox = shapes[i].px+shapes[i].length;
                 shapes[i].oy = shapes[i].py+shapes[i].length;
             }
-
-            /*REP(i, num_queries)
-            {
-                scanf("%d %d", &q.x, &q.y);
-                REP(i, num_shapes)
-                {
-                    else
-                    {
-                    a = */
         }
-        //dump();
+        /*dump();*/
         {
-            //process input
+        
             int qx, qy;
             int r = 0, g = 0, b = 0;
-            bool done = 1;
             int count = 0;
             int a = 0, c = 0;
             while (num_queries-- > 0)
             {
-                //check boundaries
+                
                 scanf("%d %d", &qx, &qy);
+                int i;
                 REP(i, num_shapes)
                 {
                     if (shapes[i].type[0] == 'S')
@@ -99,8 +79,7 @@ int main()
                               && (qx >= shapes[i].px && qx <= (shapes[i].ox)))))
                         {
                             r = g = b = 0;
-                            done = 0;
-                            break;
+                            goto end;
                         }
                         else if ((qx > shapes[i].px && qy > shapes[i].py &&
                                   qx < (shapes[i].ox) && qy < (shapes[i].oy)))
@@ -119,8 +98,7 @@ int main()
                         if (a == c)
                         {
                             r = g = b = 0;
-                            done = 0;
-                            break;
+                            goto end;
                         }
                         else if (a < c)
                         {
@@ -139,13 +117,12 @@ int main()
                     g = (2*g+count)/(2*count);
                     b = (2*b+count)/(2*count);
                 }
-                else if (done)
+                else
                     r = g = b = 255;
-               
+            end:
                 printf("(%d, %d, %d)\n", r, g, b);
                 
                 r = g = b = 0;
-                done = 1;
                 count = 0;
             }
         }
