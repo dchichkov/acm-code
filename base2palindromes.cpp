2d55a30ef0b58c8c6afa16165bf741a9abed4999
Matthew <mgavin2@tigers.lsu.edu>
2012-09-27 15:11:16 -0500

    10 millionth base2 palindrome

--- base2palindromes.cpp
+++ base2palindromes.cpp
@@ -6,7 +6,9 @@
 #include <cmath>
 #include <bitset>
 #include <algorithm>
-
+#include <ctime>
+#include <sys/time.h>
+#include <unistd.h>
 using namespace std;
 
 #define DEBUG
@@ -33,14 +35,16 @@ template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //change max
 #define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
 
 /*global variables*/
-typedef pair<int, vector<int> > bit_factors;
+typedef pair<int, vector<unsigned long long> > bit_factors;
 //.first = # bits
 //.second = factor
 vector<bit_factors> factors;
 
-vector<int> ans;
+vector<unsigned long long> ans;
 
 int num;
+
+timeval a, b;
 /*global variables*/
 
 void dump()
@@ -72,35 +76,46 @@ void precompute()
 	factors.push_back(two);
 
 	int shft = 0;
-	const int UPTO_NUM_BITS = 31;
+    int pal_count = 2;
+	const int UPTO_NUM_BITS = 50;
 	FOR(i, 3 /*bits*/, UPTO_NUM_BITS)
 	{
-		{
-			bit_factors bit_pals;
-			bit_pals.first = i;
-			int base = 1;
-			base <<= i-1;
-			base += 1;
-			bit_pals.second.push_back(base);
-			for ( size_t it = (i%2) ? 0 : 1; it < factors.size(); it += 2)
-			{
-				debug(it, TAB);
-				debug(i, TAB); debug(factors[it].first, TAB);
+        bit_factors bit_pals;
+        bit_pals.first = i;
+        int base = 1;
+        base <<= i-1;
+        base += 1;
+        bit_pals.second.push_back(base);
+        pal_count++;
+        for ( size_t it = (i%2) ? 0 : 1; it < factors.size(); it += 2)
+        {
+            debug(it, TAB);
+            debug(i, TAB); debug(factors[it].first, TAB);
                 
-				shft = (i - factors[it].first) / 2;
+            shft = (i - factors[it].first) / 2;
                 
-				debug(shft, endl);
+            debug(shft, endl);
                 
-				for ( vi::iterator jt = factors[it].second.begin(); jt != factors[it].second.end(); ++jt)
-				{
-					//inttobin((base | ((*jt) << shft)));
-					debug(*jt, TAB); debug((base | ((*jt) << shft)), endl);
+            for ( vector<unsigned long long>::iterator jt = factors[it].second.begin(); jt != factors[it].second.end(); ++jt)
+            {
+                //inttobin((base | ((*jt) << shft)));
+                debug(*jt, TAB); debug((base | ((*jt) << shft)), endl);
                     
-					bit_pals.second.push_back(base | ((*jt) << shft));
-				}
-			}
+                bit_pals.second.push_back(base | ((*jt) << shft));
+                pal_count++;
+                if (pal_count >= 10000000)
+                    goto done;
+            }
+        }
+
+        done:
 			factors.push_back(bit_pals);
-		}
+            if (pal_count >= 10000000)
+            {
+                gettimeofday(&b, 0);
+                printf("pal_count: %d ; num_bits : %d ; time: %lds\n", pal_count, i, b.tv_sec - a.tv_sec);
+                break;
+            }
 	}
 }
 
@@ -114,17 +129,20 @@ bool getInput()
 void process()
 {
     //process input
-    printf("%d\n", ans[num-1]);
+    if (num > ans.size()) printf("Go higher dum dum\n");
+    else
+        printf("%lld\n", ans[num-1]);
 }
 
 int main()
 {
-
+    gettimeofday(&a, 0);
+    ans.reserve(10000000);
     precompute();
 
 	{
 		for (vector<bit_factors>::iterator it = factors.begin(); it != factors.end(); ++it)
-			for (vi::iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+			for (vector<unsigned long long>::iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
 				ans.push_back(*jt);
 	}
 	//sort(ans.begin(), ans.end()); //not sure if needed, but what the hey, qsort an already sorted list (update: nope not needed)
