6d333764f68c1ed88e29d4fc8290237b8d10599f
Matthew <mgavin2@tigers.lsu.edu>
2012-10-09 12:34:49 -0500

    omfg... omfg.

--- 10800_notkindograph.cpp
+++ 10800_notkindograph.cpp
@@ -4,11 +4,12 @@
 #include <cstring>
 #include <cstdlib>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 #define DEBUG
-//#undef DEBUG //uncomment this line to pull out print statements
+#undef DEBUG //uncomment this line to pull out print statements
 #ifdef DEBUG
 #define TAB '\t'
 #define debug(a, end) cout << #a << ": " << a << end
@@ -31,19 +32,20 @@ template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //change max
 #define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
 
 /*global variables*/
-char graph[150][150];
+char graph[150][80];
 char line[60];
-int top;
+int bot, top;
 /*global variables*/
 
 void dump()
 {
     //dump data
-    REP(i, top-1)
+    for (int i = top; i > bot-2; --i)
     {
-        REP(j, strlen(line)+3)
+        if (strlen(&graph[75-i][0]) == 0) continue;
+        REP(j, strlen(&graph[75-i][0]))
         {
-            printf("%c", graph[i][j]);
+            printf("%c", graph[75-i][j]);
         }
         printf("\n");
     }
@@ -51,7 +53,7 @@ void dump()
 
 bool getInput()
 {
-    CL2d(graph, ' ', 150, 150);
+    CL2d(graph, 0, 150, 80);
     CL(line, 0);
     //get input
     scanf("%50s ", line);
@@ -61,80 +63,86 @@ bool getInput()
 void process()
 {
     //process input
-    top = 0;
+    bot = top = 0;
     int row = 0;
-    
-    REP(i, strlen(line))
-    {
-        switch (line[i])
-        {
-        case 'R':
-            ++row;
-            if (row > 0 && row > top )
-                ++top;
-            break;
-        case 'C':
-            break;
-        case 'F':
-            --row;
-            if (row < 0)
-                ++top;
-            break;
-        }
-    }
-    debug(top, endl);
-    debug(row, endl);
-    /*if (top < 0)
-        row = -(top) + 1;
-    else
-    {
-        row = 1;
-        top += 1;
-        }*/
-    
-    top = abs(top);
-    debug(top, endl);
-
-    REP(i, top)
-        graph[i][0] = '|';
-    graph[top][0] = '+';
-
-    REP(i, strlen(line))
-        graph[top][i+1] = '-';
-
 
     REP(i, strlen(line))
     {
         switch (line[i])
         {
         case 'R':
-            graph[top-row][i+2] = '/';
+            graph[75-row][i+2] = '/';
+            REP(m, i+2)
+                if (graph[75-row][i+2-m] == 0)
+                    graph[75-row][i+2-m] = ' ';
             row++;
+            top = max(top, row);
             break;
         case 'C':
-            graph[top-row][i+2] = '_';
+            top = max(top, row);
+            bot = min(bot, row);
+            graph[75-row][i+2] = '_';
+            REP(m, i+2)
+                if (graph[75-row][i+2-m] == 0)
+                    graph[75-row][i+2-m] = ' ';
             break;
         case 'F':
             row--;
-            graph[top-row][i+2] = '\\';
+            graph[75-row][i+2] = '\\';
+            REP(m, i+2)
+                if (graph[75-row][i+2-m] == 0)
+                    graph[75-row][i+2-m] = ' ';
+            bot = min(bot, row);
             break;
         }
     }
-    top += 2;
+    int c = 0;
+    /*FOR(i, bot-1, top)
+    {
+        REP(j, strlen(line)+3)
+        {
+            graph[75-i][j] = (graph[75-i][j] == 0) ? ' ' : graph[75-i][j];
+        }
+    }*/
+    
+    FOR(i, bot, top+1)
+    {
+        REP(j, strlen(line)+3)
+        {
+            if (graph[75-i][j] != 0)
+            {
+                c++;
+                break;
+            }
+        }    
+    }
+    
+    graph[75-bot+1][0] = '+';
+    REP(i, c)
+        graph[75-bot-i][0] = '|';
+
+    FOR(i, 1, strlen(line)+3)
+        graph[75-bot+1][i] = '-';
+    debug(c, endl);
+    debug(top, endl);
+    debug(row, endl);
+    debug(bot, endl);
     dump();
 }
 
 int main()
 {
-    int nc;
+    int nc, count = 0;
     scanf("%d\n", &nc);
     while (nc-- > 0)
     {
         getInput();
+
+        printf("Case #%d:\n", ++count);
+        
         process();
 
-        if (nc != 0)
-            printf("\n");
+        printf("\n");
     }
 
     return 0;
