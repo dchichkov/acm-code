d82656eb1f8fa458baa4c5b181d54b06c9fb6ea3
Matthew <mgavin2@tigers.lsu.edu>
2012-10-10 20:00:37 -0500

    cant think..

---
+++ 579_clockhands.cpp
@@ -0,0 +1,78 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+#define DEBUG
+//#undef DEBUG //uncomment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef long long int64; //for clarity
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } //change min
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //change max
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int hr, minu;
+const double deg = (double)180 / 3600;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    scanf("%d:%d", &hr, &minu);
+    if (hr == 0 and minu == 0) return false;
+    return true;
+}
+
+void process()
+{
+    //process input
+    int thr = abs(12-hr);
+    int tmin;
+    if (hr >= 6) tmin = 3600 - (thr*60) - minu;
+    else tmin = (thr*60) + minu;
+
+    printf("%0.3lf\n", tmin*deg);
+    
+}
+
+int main()
+{
+    while (getInput())
+    {
+
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
