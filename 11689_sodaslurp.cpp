0bba22d855e85d68469ef58357efff4bde5f85cb
Matthew <mgavin2@tigers.lsu.edu>
2012-10-17 13:53:27 -0500

    oh boy lol

---
+++ 11689_sodaslurp.cpp
@@ -0,0 +1,81 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+#define DEBUG
+#undef DEBUG //uncomment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef long long int64; //for clarity
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } //change min
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //change max
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int start, during, cost;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    scanf("%d %d %d", &start, &during, &cost);
+    return true;
+}
+
+void process()
+{
+    //process input
+    int total = 0;
+    int rem = start + during;
+    do
+    {
+        total += rem/cost;
+        rem = rem%cost + rem/cost;
+        debug(total, TAB); debug(rem, endl);
+    } while (rem >= cost);
+        
+    printf("%d\n", total);
+}
+
+int main()
+{
+    int nc;
+    scanf("%d", &nc);
+    while (nc-- > 0)
+    {
+        getInput();
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
