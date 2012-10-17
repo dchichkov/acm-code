6ec9b5746773f99b5a5af597a6eca35e4da84d8b
Matthew <mgavin2@tigers.lsu.edu>
2012-10-17 11:37:33 -0500

    argh

--- crc_128.java
+++ crc_128.java
@@ -3,27 +3,34 @@ import java.util.*;
 
 public class crc_128
 {
-    private static final String g = "34943";
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        String line = new String();
+        String line;
         BigInteger n;
+        
         int p;
+        BigInteger wtf = new BigInteger("34943");
+        int g = (1<<16)%34943;
         while (in.hasNext())
         {
             line = in.nextLine();
-            if (line.compareTo("#") == 0) break;
+            if (line.compareTo("#") == 0 || line.indexOf("#") != -1) break;
             if (line.length() == 0)
             {
                 System.out.printf("00 00\n");
             }
             else
             {
-                n = (new BigInteger(line.getBytes())).shiftLeft(16);
-                p = (34943 - (n.mod(new BigInteger(g))).intValue())%34943;
+                n = (new BigInteger(line.getBytes())).mod(wtf);
+                p = (34943 - ((n).intValue() * g % 34943));
                 System.out.printf("%02X %02X\n", p>>8 , p%256);            
             }
         }
     }
 }
+/* (a*b)%c == (a%c * b%c)%c
+ *
+ * uhhh which ... doesn't apply?
+ * 2^16 % 34943
+ * 34943 - (line.getBytes() % 34943) * 2^16 % 34943 */
