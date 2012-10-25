a32930da84664fbd1e03632379001f698011e977
Matthew <matthew@matthew.(none)>
2012-10-24 21:46:08 -0500

    lol woops

---
+++ 895_wp.cpp
@@ -0,0 +1,108 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef long long int64; //for clarity
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } //change min
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //change max
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+vector<string> dict;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    string line;
+    getline(cin, line);
+    while (line != "#")
+    {
+        dict.push_back(line);
+        getline(cin, line);
+    }
+    return true;
+}
+
+void process()
+{
+    //process input
+    string line;
+    int num_m = 0;
+    bool possible = false;
+    getline(cin, line);
+    while (line != "#")
+    {
+        num_m = 0;
+        for (vector<string>::iterator it = dict.begin(); it != dict.end();
+             ++it)
+        {
+            possible = false;
+            REP(i, line.length())
+            {
+                debug(line[i], TAB);
+                debug(count(it->begin(), it->end(), line[i]), TAB);
+                debug(count(line.begin(), line.end(), line[i]), endl);
+                if (line[i] != ' ' &&
+                    count(it->begin(), it->end(), line[i]) ==
+                    count(line.begin(), line.end(), line[i]))
+                {
+                    if (i == line.length()-1)
+                        possible = true;
+                }
+                else if (line[i] == ' ')
+                    ;
+                else
+                    break;
+            }
+            if (possible)
+                num_m += 1;
+        }
+        printf("%d\n", num_m);
+        getline(cin, line);
+    }
+}
+
+int main()
+{
+    {
+        getInput();
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
