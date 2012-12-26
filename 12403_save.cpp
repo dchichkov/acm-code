2dda28b692786fb1a8e687524ca28cb8eb223e90
Matthew <mgavin2@tigers.lsu.edu>
2012-12-26 15:09:24 -0600

    12403

---
+++ 12403_save.cpp
@@ -0,0 +1,80 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int amt;
+char line[1000];
+char command[20];
+long long total;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    fgets(line, 1000, stdin);
+    return true;
+}
+
+void process()
+{
+    //process input
+    sscanf(line, "%s %d", command, &amt);
+    switch (command[0])
+    {
+    case 'd':
+        total += amt;
+        break;
+    case 'r':
+        printf("%lld\n", total);
+    }
+}
+
+int main()
+{
+    int nc;
+    scanf("%d\n", &nc);
+    while (nc-- > 0)
+    {
+        getInput();
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+        amt = 0;
+        
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
