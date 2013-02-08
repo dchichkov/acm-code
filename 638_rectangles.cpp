1085c58fb1e6a7ca64ae09f520d968f1e1e0c5f6
Matthew <mgavin2@tigers.lsu.edu>
2013-02-08 10:47:47 -0600

    solved 638

--- 638_rectangles.cpp
+++ 638_rectangles.cpp
@@ -1,3 +1,12 @@
+/* Class:   CSC 2700 Programming Competitions
+ * Prof:    Isaac Traxler
+ * Name:    Matthew Gavin
+ * Problem: 638 - Rectangles
+ *
+ * Note: Only 3 loops are required... I don't know why I was trying to recursively backtrack ;)
+ *       This is a very simple problem!
+ */ 
+
 #include <cstdio>
 #include <iostream>
 #include <vector>
@@ -9,7 +18,7 @@
 
 using namespace std;
 
-#define DEBUG  //comment this line to pull out print statements
+//#define DEBUG  //comment this line to pull out print statements
 #ifdef DEBUG
 #define TAB '\t'
 #define debug(a, end) cout << #a << ": " << a << end
@@ -19,9 +28,7 @@ using namespace std;
 #define dbg(end)
 #endif
 
-typedef pair<int, int> point;
 typedef vector<int> vi; //?
-typedef vector<point> vp; //?
 
 #define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
 #define SORT(c) sort((c).begin(),(c).end())   
@@ -31,9 +38,15 @@ typedef vector<point> vp; //?
 #define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
 
 /*global variables*/
-char grid[51][51];
-int num_pt;
 
+struct point {
+    char ltr;
+    int x;
+    int y;
+};
+
+int num_pt;
+vector<point> pts;
 vector<string> rects;
 /*global variables*/
 
@@ -45,80 +58,69 @@ void dump()
 bool getInput()
 {
     //get input
-    scanf("%d", &num_pt);
+    scanf("%d ", &num_pt);
     if (num_pt == 0) return false;
-
-    char pt;
-    int x, y;
+    //debug(num_pt, endl);
+    point p;
     REP(i, num_pt)
     {
-        scanf("%c %d %d", &pt, &x, &y);
-        grid[x][y] = pt;
+        scanf("%c %d %d ", &p.ltr, &p.x, &p.y);
+        //debug(p.ltr, TAB); debug(p.x, TAB); debug(p.y, endl);
+        pts.push_back(p);
     }
     
     return true;
 }
 
-void find_rect(char start, int i, int j, int dir)
-{
-    switch (dir)
-    {
-    case 1:
-        //right
-        FOR(x, i, 51)
-        {
-            if (isalpha(grid[i][x]))
-            {
-                rects.push_back("");
-                rects[rects.size()-1] += grid[i][x];
-                find_rect(start, i, x, 2);
-            }   
-        }
-        break;
-    case 2:
-        //down
-        for (int y = i; y >= 0; --y)
-        {
-            if (isalpha(grid[y][j]))
-            {
-                rects[rects.size()-1] += grid[y][j];
-                find_rect(start, y, j, 3);
-            }
-        }
-        break;
-    case 3:
-        //left
-        for (int x = j; x >= 0; --x)
-        {
-            if (isalpha(grid[i][x]))
-            {
-                rects[rects.size()-1] += grid[i][x];
-                find_rect(start, i, x, 4);
-            }
-        }
-        break;
-    case 4:
-        //up
-        FOR(y, j, 51)
-        {
-            if (isalpha(grid[y][j]) && grid[y][j] != start)
-                rects.erase(rects.begin()+rects.size()-1);
-        }
-        break;
-    }
-}
-
 void process()
 {
     //process input
-    REP(i, 51)
+    string r;
+    REP(i, pts.size())
     {
-        REP(j, 51)
+        r += pts[i].ltr;
+        FOR(j, 0, pts.size())
         {
-            if (isalpha(grid[i][j])) find_rect(grid[i][j], i, j, 1);
+            //if (j == i) continue;
+            //right
+            if (pts[j].x > pts[i].x && pts[j].y == pts[i].y)
+            {
+                //found right
+                r += pts[j].ltr;
+                debug(r, endl);
+                FOR(k, 0, pts.size())
+                {
+                    //if (k == i || k == j) continue;
+                    //go down
+                    if (pts[k].y < pts[j].y && pts[k].x == pts[j].x)
+                    {
+                        //found down
+                        r += pts[k].ltr;
+                        debug(r, endl);
+                        FOR(l, 0, pts.size())
+                        {
+                            //if (l == i || l == j || l == k) continue;
+                            //go left
+                            if ((pts[l].x < pts[k].x && pts[l].y == pts[k].y) &&
+                                (pts[l].x == pts[i].x && pts[i].y > pts[l].y))
+                            {
+                                debug(r, endl);
+                                //found left and compared with the starting point
+                                r += pts[l].ltr;
+                                rects.push_back(r);
+                                r.erase(3, 1); //take out the ltr for the next iter
+                            }
+                        }
+                        r.erase(2, 1); //take out the ltr for the next iter
+                    }
+                }
+                r.erase(1, 1); //take out the ltr for the next iter
+            }
         }
+        r.clear(); //clear the str for the next iter
     }
 
+    
     
     SORT(rects);
     if (rects.size() == 0)
@@ -128,12 +130,12 @@ void process()
         printf("\n");
         REP(i, (int)rects.size())
         {
-            if (i % 10 == 0)
+            if (i % 10 == 0 && i != 0)
                 printf("\n");
-            else
-                cout << " " << rects[i];
+            cout << " " << rects[i];
         }
     }
+    printf("\n");
 }
 
 int main()
@@ -141,12 +143,12 @@ int main()
     int nc = 0;
     while (getInput())
     {
-        CL2d(grid, 0, 51, 51);
-        printf("Point set %d:", nc++);
+        printf("Point set %d:", ++nc);
         process();
 
         /*CLEAR GLOBAL VARIABLES!*/
         rects.clear();
+        pts.clear();
         /*CLEAR GLOBAL VARIABLES!*/
     }
 
