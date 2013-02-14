475c1205afb03732402f31e1ea4c6790f31350ca
Matthew <mgavin2@tigers.lsu.edu>
2013-02-13 18:21:22 -0600

    triangles wooh

---
+++ 11936_lazy.cpp
@@ -0,0 +1,73 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+/*global variables*/
+int sides[3];
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    scanf("%d %d %d", &sides[0], &sides[1], &sides[2]);
+    return true;
+}
+
+void process()
+{
+    //process input
+    sort(sides, sides+3);
+    if (sides[0] + sides[1] > sides[2])
+        printf("OK");
+    else
+        printf("Wrong!!");
+    printf("\n");
+}
+
+int main()
+{
+    int nc = 0;
+    scanf("%d ", &nc);
+    while (nc-- > 0)
+    {
+        getInput();
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
