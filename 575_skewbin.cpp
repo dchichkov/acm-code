a2ab02dbc775ecff786f8b4333f465191ce6a90f
Matthew <mgavin2@tigers.lsu.edu>
2013-03-05 13:28:45 -0600

    solved easy 575

---
+++ 575_skewbin.cpp
@@ -0,0 +1,73 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+string line;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    getline(cin, line);
+    if (line.compare("0") == 0) return false;
+    return true;
+}
+
+void process()
+{
+    //process input
+    int p = 0;
+    REP(i, line.length())
+    {
+        p += (line[line.length()-1-i]-'0') * (pow(2, i+1) - 1);
+    }
+    printf("%d\n", p);
+}
+
+int main()
+{
+    while (getInput())
+    {
+
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
