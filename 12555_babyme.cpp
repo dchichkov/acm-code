15f2ddb70728f2cb0163ac13f8257c18fd42a9f9
Matthew <mgavin2@tigers.lsu.edu>
2013-03-07 18:52:16 -0600

    screw you empty commit message

---
+++ 12555_babyme.cpp
@@ -0,0 +1,75 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    int d, e;
+    char n, m;
+    scanf("%d%c%d%c ", &d, &n, &e, &m);
+    debug(d, TAB); debug(n, TAB);    debug(e, TAB); debug(m, endl);
+    return true;
+}
+
+void process()
+{
+    //process input
+}
+
+int main()
+{
+    int nc = 0;
+    int count = 0;
+
+    scanf("%d ", &nc);
+
+    while (nc-- > 0)
+    {
+        printf("Case %d: ", ++count);
+        getInput();
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
