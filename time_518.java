47baba28124aa1f31e0571b9d40def0ecfffa2e7
Matthew <mgavin2@tigers.lsu.edu>
2013-03-21 13:55:56 -0500

    TLE

--- time_518.java
+++ time_518.java
@@ -23,8 +23,8 @@ public class time_518
             t = input.nextLong();
             s = input.next();
 
-            int y, mo, da, h, mi, se;
-            y = mo = da = h = mi = se = 0;
+            int y, mo, moo, da, h, mi, se;
+            y = mo = da = h = mi = se = moo= 0;
             
             if (a.isLeapYear(a.get(Calendar.YEAR)) &&
                 b.get(Calendar.MONTH) > 2 && a.get(Calendar.MONTH) <= 2 &&
@@ -32,7 +32,7 @@ public class time_518
             {
                 da++;
             }
-            
+            moo = a.get(Calendar.MONTH);
             while (a.get(Calendar.SECOND) != b.get(Calendar.SECOND))
             {
                 a.add(Calendar.SECOND, 1);
@@ -48,16 +48,6 @@ public class time_518
                 a.add(Calendar.HOUR, 1);
                 h++;
             }
-            while (a.get(Calendar.DAY_OF_YEAR) != b.get(Calendar.DAY_OF_YEAR))
-            {
-                a.add(Calendar.DAY_OF_YEAR, 1);
-                da++;
-            }
-            while (a.get(Calendar.MONTH) != b.get(Calendar.MONTH))
-            {
-                a.add(Calendar.MONTH, 1);
-                mo++;
-            }
             while (a.get(Calendar.YEAR) != b.get(Calendar.YEAR))
             {
                 a.add(Calendar.YEAR, 1);
@@ -66,9 +56,41 @@ public class time_518
             }
             while (a.get(Calendar.DAY_OF_YEAR) != b.get(Calendar.DAY_OF_YEAR))
             {
-                a.add(Calendar.DAY_OF_YEAR, 1);
-                da++;
+                if (a.get(Calendar.DAY_OF_YEAR) < b.get(Calendar.DAY_OF_YEAR))
+                {
+                    a.add(Calendar.DAY_OF_YEAR, 1);
+                    da++;
+                }
+                else if (a.get(Calendar.DAY_OF_YEAR) > b.get(Calendar.DAY_OF_YEAR))
+                {
+                    a.add(Calendar.DAY_OF_YEAR, -1);
+                    da--;
+                }
+                if (a.get(Calendar.MONTH) != moo)
+                {
+                    mo++;
+                    moo = a.get(Calendar.MONTH);
+                }
             }
+            while (a.get(Calendar.HOUR_OF_DAY) != b.get(Calendar.HOUR_OF_DAY))
+            {
+                if (a.get(Calendar.HOUR_OF_DAY) < b.get(Calendar.HOUR_OF_DAY))
+                {
+                    a.add(Calendar.HOUR_OF_DAY, 1);
+                    h++;
+                }
+                else if (a.get(Calendar.HOUR_OF_DAY) > b.get(Calendar.HOUR_OF_DAY))
+                {
+                    a.add(Calendar.HOUR_OF_DAY, -1);
+                    h--;
+                }
+            }
+
+            if (a.compareTo(b) != 0)
+            {
+                System.out.println(a);
+                System.out.println(b); }
+
             if (s.compareTo("second") == 0)
             {
                 ans = ((y*365*24*60*60) +
