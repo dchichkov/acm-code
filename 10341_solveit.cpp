94bc6c3dfd61f3010fd445f5238d9d9a424047cb
mag <matthew.ashton3@gmail.com>
2013-09-05 02:07:21 -0600

    book said bisection method...

---
+++ 10341_solveit.cpp
@@ -0,0 +1,107 @@
+/* Class:   CSC 2700 Programming Competitions
+ * Prof:    Isaac Traxler
+ * Name:    Matthew Gavin
+ * Problem: 10341 - Solve It
+ *
+ * CP2 book said bisection method. 
+ */
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi; //?
+typedef vector<point> vp; //?
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int p, q, r, s, t, u;
+
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    if (feof(stdin)) return false;
+    scanf("%d %d %d %d %d %d ", &p, &q, &r, &s, &t, &u);
+    return true;
+}
+
+void process()
+{
+    //process input
+    double x = 1.0/2;
+    char s;
+    double result;
+    while (true)
+    {
+        result = p * exp(-x) +
+            q * sin(x) +
+            r * cos(x) +
+            s * tan(x) +
+            t * x * x +
+            u;
+        if ( result < 0 )
+        {
+            x = x-(1-x)*1.0/2;
+            debug(x, TAB); debug(result, endl);
+            if (x < 0) break;
+        }
+        else if (result > 0)
+        {
+            x = x+(1-x)*1.0/2;
+            debug(x, TAB); debug(result, endl);
+            if (x > 1) break;
+        }
+        else break;
+        cin >> s;
+    };
+
+    if (x < 0 || x > 1) printf("No solution");
+    else printf("%0.4lf", x);
+
+    printf("\n");
+}
+
+int main()
+{
+    while (getInput())
+    {
+
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
