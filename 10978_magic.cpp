001caac47be0472913d1588b4c6d2fab9594375d
mag <matthew.ashton3@gmail.com>
2014-07-09 13:13:16 -0500

    finally

--- 10978_magic.cpp
+++ 10978_magic.cpp
@@ -4,10 +4,11 @@
 #include <cstring>
 #include <cstdlib>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-#define DEBUG  //comment this line to pull out print statements
+//#define DEBUG  //comment this line to pull out print statements
 #ifdef DEBUG
 #define TAB '\t'
 #define debug(a, end) cout << #a << ": " << a << end
@@ -29,58 +30,66 @@ typedef vector<point> vp; //?
 #define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
 
 /*global variables*/
-char cardarr[52][2];
-int taken[52];
-int N;
+char cards[52][3];
+int c;
 /*global variables*/
 
 void dump()
 {
     //dump data
+    REP(i, c)
+    {
+        printf("%d[%s] ", i+1, cards[i]);
+    }
+    puts("");
 }
 
 bool getInput()
 {
     //get input
-    scanf("%d", &N);
-    if (N == 0) return false;
+    int j = 0, k;
+    scanf("%d ", &c);
+    if (c == 0) return false;
+    char cs[2], word[20];
+    REP(i, c)
+    {
+        scanf("%2s %20s\n ", cs, word);
+        debug(cs, TAB); debug(word, TAB);
+        k = strlen(word);
+        while (k-- > 0)
+        {
+            j %= c;
+            ++j;
+            while(strlen(cards[j-1]) != 0) { j %= c;  ++j;}
+        }
+        debug(j, endl);
+        strcpy(cards[(j-1)], cs);
+        dbg( dump(); );
+    }
+    
     return true;
 }
 
 void process()
 {
-    int pos = -1, w = 0;
-    string card, word;
     //process input
-    REP(i, N)
+    printf("%s", cards[0]);
+    FOR(i, 1, c)
     {
-        cin >> card >> word;
-        pos = (pos + word.length()) % N;
-        while (taken[pos]) pos = (pos + 1) % N;
-        debug(card, TAB); debug(word, TAB); debug(pos, endl);
-        cardarr[pos][0] = card[0];
-        cardarr[pos][1] = card[1];
-        taken[pos] = 1;
-
+        printf(" %s", cards[i]);        
     }
-    printf("%s", cardarr[0]);
-    FOR(i, 1, N)
-    {
-        printf(" %s", cardarr[i]);
-    }
-    printf("\n");
+    puts("");
 }
 
 int main()
 {
+    CL2d(cards, 0, 52, 3);
     while (getInput())
     {
-
         process();
 
         /*CLEAR GLOBAL VARIABLES!*/
-        CL2d(cardarr, 0, 52, 2);
-        CL(taken, 0);
+        CL2d(cards, 0, 52, 3);
         /*CLEAR GLOBAL VARIABLES!*/
     }
 
