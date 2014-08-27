979b6837fa42a2a489392d7c6b9e568469e140f4
mag <matthew.ashton3@gmail.com>
2014-08-27 17:18:08 -0500

    another easy one

---
+++ 10226_hardwood.cpp
@@ -0,0 +1,83 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi;
+typedef vector<point> vp;
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int total = 0;
+map<string, int> trees;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    string line;
+    while(getline(cin, line), line.compare("") != 0)
+    {
+        total++;
+        if (trees.find(line) == trees.end())
+            trees[line] = 1;
+        else trees[line]++;
+        if (cin.eof()) break;
+    }
+    return true;
+}
+
+void process()
+{
+    //process input
+    for (map<string, int>::iterator it = trees.begin(); it != trees.end(); ++it)
+    {
+        printf("%s %0.4lf\n", it->first.c_str(), ((double)it->second/total)*100);
+    }
+}
+
+int main()
+{
+    int nc;
+    scanf("%d ", &nc);
+    while (nc-- > 0)
+    {
+        getInput();
+        process();
+        if (nc != 0) puts("");
+        /*CLEAR GLOBAL VARIABLES!*/
+        total = 0;
+        trees.clear();
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
