43ecbaa0380dd26350ff1a57a3bca1a5198ec8fd
mag <matthew.ashton3@gmail.com>
2014-09-09 20:25:20 -0500

    wee

---
+++ spoj/15708_divisibility.cpp
@@ -0,0 +1,77 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi;
+typedef vector<point> vp;
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int n, x, y;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    scanf("%d %d %d ", &n, &x, &y);
+    return true;
+}
+
+void process()
+{
+    //process input
+    int i = 2;
+    printf("%d", x);
+    while ((i*x) < n)
+    {
+        if (((i*x) % y) != 0)
+            printf(" %d", (i*x));
+        i++;
+    }
+    puts("");
+}
+
+int main()
+{
+    int nc;
+    scanf("%d ", &nc);
+    while (nc-- > 0)
+    {
+        getInput();
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
---
+++ spoj/15710_itsum.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi;
+typedef vector<point> vp;
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int a, b;
+/*global variables*/
+
+void dump()
+{
+    //dump data
+}
+
+bool getInput()
+{
+    //get input
+    scanf("%d %d ", &a, &b);
+    return true;
+}
+
+void process()
+{
+    //process input
+    long long x = 0;
+    FOR(i, a, b+1)
+    {
+        x += i*i;
+    }
+    printf("%lld\n", x);
+}
+
+int main()
+{
+    //while ()
+    {
+        getInput();
+        process();
+
+        /*CLEAR GLOBAL VARIABLES!*/
+
+        /*CLEAR GLOBAL VARIABLES!*/
+    }
+
+    return 0;
+}
