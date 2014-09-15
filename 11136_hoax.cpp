08fb5ff61f005db05fa3fdef37dcd9c8e2db8d0e
mag <matthew.ashton3@gmail.com>
2014-09-15 09:52:05 -0500

    git status log becoming large

--- 11136_hoax.cpp
+++ 11136_hoax.cpp
@@ -5,10 +5,11 @@
 #include <cstdlib>
 #include <cmath>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
-#define DEBUG  //comment this line to pull out print statements
+//#define DEBUG  //comment this line to pull out print statements
 #ifdef DEBUG
 #define TAB '\t'
 #define debug(a, end) cout << #a << ": " << a << end
@@ -19,8 +20,8 @@ using namespace std;
 #endif
 
 typedef pair<int, int> point;
-typedef vector<int> vi; //?
-typedef vector<point> vp; //?
+typedef vector<int> vi;
+typedef vector<point> vp;
 
 #define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
 #define SORT(c) sort((c).begin(),(c).end())   
@@ -30,7 +31,7 @@ typedef vector<point> vp; //?
 #define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
 
 /*global variables*/
-vi samples;
+unsigned long long ans;
 /*global variables*/
 
 void dump()
@@ -41,38 +42,39 @@ void dump()
 bool getInput()
 {
     //get input
-    int num_in;
-    scanf("%d ", &num_in);
-    if (num_in == 0) return false;
+    int days;
+    scanf("%d ", &days);
+    if (days == 0) return false;
+    multiset<int> receipts;
 
-    int a;
-    REP(i, num_in)
+    int a, b;
+    REP(i, days)
     {
         scanf("%d ", &a);
-        samples.push_back(a);
+        REP(j, a)
+        {
+            scanf("%d ", &b);
+            receipts.insert(b);
+        }
+        if (receipts.size() > 1)
+        {
+            debug(*(receipts.begin()), TAB);
+            debug(*(receipts.rbegin()), endl);
+            ans += *(receipts.rbegin()) - *(receipts.begin());
+
+            receipts.erase(--receipts.end());
+            receipts.erase(receipts.begin());
+        }
     }
+    
+    
     return true;
 }
 
 void process()
 {
     //process input
-    int total = 0;
-    samples.insert(samples.begin(), samples.back());
-    samples.push_back(samples[1]);
-    int mx = samples[0];
-    for (int i = 0; i < samples.size(); ++i)
-    {
-        if (mx < samples[i])
-            mx = samples[i];
-        else if (mx > samples[i])
-        {
-            mx = samples[i];
-            total++;
-        }
-        debug(mx, endl);
-    }
-    printf("%d\n", total);
+    printf("%llu\n", ans);
 }
 
 int main()
@@ -83,7 +85,7 @@ int main()
         process();
 
         /*CLEAR GLOBAL VARIABLES!*/
-        samples.clear();
+        ans = 0;
         /*CLEAR GLOBAL VARIABLES!*/
     }
 
