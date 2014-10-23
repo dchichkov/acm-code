dc4e19d07e96ca29d8357dfc8d84f732c1765d1d
mag <matthew.ashton3@gmail.com>
2014-10-23 15:45:06 -0500

    ugh, partial solution that couldve been easier

---
+++ 722_lakes.cpp
@@ -0,0 +1,108 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+//#define DEBUG  //comment this line to pull out print statements
+#ifdef DEBUG
+#define TAB '\t'
+#define debug(a, end) cout << #a << ": " << a << end
+#define dbg(end) end
+#else
+#define debug(a, end)
+#define dbg(end)
+#endif
+
+typedef pair<int, int> point;
+typedef vector<int> vi;
+typedef vector<point> vp;
+
+#define UN(v) SORT(v),v.erase(unique(v.begin(),v.end()),v.end())   
+#define SORT(c) sort((c).begin(),(c).end())   
+#define FOR(i,a,b) for (int  i=(a); i < (b); i++)    
+#define REP(i,n) FOR(i,0,n)    
+#define CL(a,b) memset(a,b,sizeof(a))
+#define CL2d(a,b,x,y) memset(a, b, sizeof(a[0][0])*x*y)
+
+/*global variables*/
+int cnt;
+char grid[120][120];
+int lx, ly;
+int mvx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int mvy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+/*global variables*/
+
+void dump()
+{
+}
+
+bool getInput()
+{
+    //get input
+    scanf("%d %d", &lx, &ly);
+    char line[200];
+    int i = 1;
+    while (!feof(stdin))
+    {
+        scanf(" ");
+        fgets(line, 200, stdin);
+        if (strlen(line) == 1 && line[0] == '\n') break;
+        else
+        {
+            FOR(j, 1, strlen(line))
+            {
+                grid[i][j] = !(line[j-1]-0x30);
+                dbg( printf("%d", grid[i][j]); );
+            }
+        }
+        dbg( puts(""); );
+        ++i;
+    }
+        
+    return true;
+}
+
+void floodfill(int x, int y)
+{
+    REP(i, 8)
+    {
+        if (grid[x+mvx[i]][y+mvy[i]] == 1)
+        {
+            grid[x+mvx[i]][y+mvy[i]] = 0;
+            cnt++;
+            floodfill(x+mvx[i], y+mvy[i]);
+        }
+    }
+}
+
+void process()
+{
+    //process input
+    floodfill(lx, ly);
+    printf("%d\n", cnt);
+}
+
+int main()
+{
+    int tc;
+    scanf("%d ", &tc);
+    CL2d(grid, 0, 120, 120);
+    while (tc-- > 0)
+    {
+        getInput();
+        process();
+        if (tc != 0) puts("");
+        /*CLEAR GLOBAL VARIABLES!*/
+        CL2d(grid, 0, 120, 120);
+        cnt = 0;
+        /*CLEAR GLOBAL VARIABLES!*/
+        
+    }
+
+    return 0;
+}
